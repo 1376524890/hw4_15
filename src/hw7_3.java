b7fb63fffcc32bdbcb7d0f4eaab0a3d57533430e
1376524890 <plk161211@163.com>
2024-04-15 14:33:57 +0800

    7.3完成

--- src/hw7_3.java
+++ src/hw7_3.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
 public class hw7_3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int[] num_array = new int[101];
+        for(int i = 0; i < 101; i++) {
+            num_array[i] = 0;
+        }
+        for(int i = 101;;){
+            i = scan.nextInt();
+            if(i != 0){
+                num_array[i]++;
+            }
+            else {
+                break;
+            }
+        }
+        for(int i = 1;i<=100;i++){
+            if(num_array[i]!=0){
+                System.out.println(i+" occurs  "+num_array[i]+" times");
+            }
+        }
+    }
 }
