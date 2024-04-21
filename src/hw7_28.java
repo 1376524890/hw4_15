fd0e1f77253c94d9aa17c4ba0a9147828a59696f
1376524890 <plk161211@163.com>
2024-04-21 18:54:43 +0800

    7.28完成

---
+++ src/hw7_28.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+public class hw7_28 {
+    public static int[] allNumber = new int[10];
+    public static void input(){
+        Scanner scan = new Scanner(System.in);
+        for (int i = 0; i < 10; i++) {
+            allNumber[i] = scan.nextInt();
+        }
+    }
+    public static void printNext(int lastID,int firstID){
+        if(lastID < 9){
+            lastID++;
+            System.out.println(allNumber[firstID]+" "+allNumber[lastID]);
+            if (lastID < 9){
+                printNext(lastID,firstID);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        input();
+        for (int i = 0; i < 10; i++) {
+            printNext(i,i);
+        }
+    }
+}
